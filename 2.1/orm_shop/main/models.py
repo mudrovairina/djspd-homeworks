a0878c6aba7bbd5194960ec63ba8457fc58b68fd
Ira Mudrova <mudrovairina18@yandex.ru>
2024-03-08 12:11:19 +0400

    Initial commit

--- 2.1/orm_shop/main/models.py
+++ 2.1/orm_shop/main/models.py
@@ -46,8 +46,26 @@ DRIVE_UNIT_CHOICES = (
 
 
 class Car(models.Model):
-    pass  # реализуйте модель
+    model = models.CharField(max_length=200)
+    year = models.IntegerField()
+    color = models.CharField(max_length=100)
+    mileage = models.IntegerField()
+    volume = models.DecimalField(max_digits=2, decimal_places=1)
+    body_type = models.CharField(max_length=20, choices=DRIVE_UNIT_CHOICES)
+    gearbox = models.CharField(max_length=20, choices=GEARBOX_CHOICES)
+    fuel_type = models.CharField(max_length=20, choices=FUEL_TYPE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="images")
+    # изображение авто, сами картинки можно взять в папке images
+
+    def __str__(self):
+        return self.model
 
 
 class Sale(models.Model):
-    pass  # реализуйте модель
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Client {self.id}'
--- 2.1/orm_shop/main/views.py
+++ 2.1/orm_shop/main/views.py
@@ -1,25 +1,39 @@
 from django.http import Http404
 from django.shortcuts import render
 
-from main.models import Car
+from main.models import Car, Sale
 
 
 def cars_list_view(request):
     # получите список авто
+    search = request.GET.get('q')
     template_name = 'main/list.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+    else:
+        cars = Car.objects.all()
+
+    context = {'cars': cars}
+    return render(request, template_name, context)  # передайте необходимый контекст
 
 
 def car_details_view(request, car_id):
     # получите авто, если же его нет, выбросьте ошибку 404
-    template_name = 'main/details.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    try:
+        template_name = 'main/details.html'
+        car = Car.objects.get(id=car_id)
+        context = {'car': car}
+        return render(request, template_name, context)
+    except Car.DoesNotExist:
+        raise Http404('Car not found')
 
 
 def sales_by_car(request, car_id):
     try:
         # получите авто и его продажи
         template_name = 'main/sales.html'
-        return render(request, template_name, {})  # передайте необходимый контекст
+        sale = Sale.objects.filter(car_id=car_id)
+        context = {'sales': sale}
+        return render(request, template_name, context)  # передайте необходимый контекст
     except Car.DoesNotExist:
         raise Http404('Car not found')
